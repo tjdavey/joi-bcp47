98ccbd89702c750b6b902408f1767d448a481158
Tristan Davey <tristan@tristandavey.com>
2020-06-24 23:06:06 +1000

    Initial commit of joi-bcp47

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # joi-bcp47
-Validate BCP47 strings with Joi
+
+[![Build Status](https://travis-ci.org/tjdavey/joi-bcp47.svg?branch=master)](https://travis-ci.org/tjdavey/joi-bcp47)
+[![Coverage Status](https://coveralls.io/repos/github/tjdavey/joi-bcp47/badge.svg)](https://coveralls.io/github/tjdavey/joi-bcp47)
+[![Known Vulnerabilities](https://snyk.io/test/github/tjdavey/joi-bcp47/badge.svg?targetFile=package.json)](https://snyk.io/test/github/tjdavey/joi-bcp47?targetFile=package.json)
+[![npm version](https://badge.fury.io/js/joi-bcp47.svg)](https://badge.fury.io/js/joi-bcp47)
+
+Provides a Joi rule to validate and parse BCP47 language tags (eg. `en-US`, `ur-PK`, `zh-Hant-TW`). 
+
+Note: Only supports Joi 16.x or higher. Does not validate the components of the BCP47 tag (eg. language or region codes)
+
+## Installation:
+
+**npm:** `npm install joi-bcp47`
+
+**yarn:** `yarn add joi-bcp47`
+
+## Usage
+
+```js
+import BaseJoi from 'joi';
+import JoiBcp47 from 'joi-bcp47';
+
+const Joi = BaseJoi.extend(JoiBcp47);
+
+Joi.bcp47().validate('en-US');
+// returns {error: null, value: 'en-US'}
+```
+
+### Rules
+
+#### `parse` - Parse the BCP47 tag into an object
+
+When added to the validation chain returns a [bcp47 parse object](https://github.com/gagle/node-bcp47/blob/master/examples/sample.js) which parses the BCP47 tag out to its individual components. 
+
+```js
+Joi.bcp47().parse().validate('hy-Latn-IT-arevela');
+/*
+    returns {
+        error: null,
+        value: {
+            langtag: {
+                language: {
+                    language: "hy",
+                    extlang: []
+                },
+                script: "Latn",
+                region: "IT",
+                variant: ["arevela"],
+                extension: [],
+                privateuse: []
+            },
+            privateuse: [],
+            grandfathered: {
+                irregular: null,
+                regular: null
+            }
+        }
+    }
+*/
+```
---
+++ test/index.test.js
@@ -0,0 +1,51 @@
+const BaseJoi = require('@hapi/joi');
+const {assert} = require('chai');
+const JoiBcp47 = require('../lib');
+
+const Joi = BaseJoi.extend(JoiBcp47);
+
+describe('string', () => {
+  describe('bcp47()', () => {
+    it('validates a basic valid bcp47 tag', () => {
+      const testValue = Joi.bcp47().validate('en-AU');
+      assert.isNotOk(testValue.error, 'should not produce an error');
+      assert.equal(testValue.value, 'en-AU');
+    });
+
+    it('validates a more complex valid bcp47 tag', () => {
+      const testValue = Joi.bcp47().validate('ms-Arab-CC'); // Malay written in Arabic script
+      assert.isNotOk(testValue.error, 'should not produce an error');
+      assert.equal(testValue.value, 'ms-Arab-CC');
+    });
+
+    it('validates a valid bcp47 tag and returns an object with parse flag enabled', () => {
+      const testValue = Joi.bcp47().parse().validate('zh-Hant-HK');
+      assert.isNotOk(testValue.error, 'should not produce an error');
+      assert.deepEqual(testValue.value, {
+        langtag: {
+          language: {language: 'zh', extlang: []},
+          script: 'Hant',
+          region: 'HK',
+          variant: [],
+          extension: [],
+          privateuse: []
+        },
+        privateuse: [],
+        grandfathered: {irregular: null, regular: null}
+      });
+    });
+
+    it('return error for a valid bcp47 tag', () => {
+      const testValue = Joi.bcp47().validate('-ABC');
+      assert.isOk(testValue.error, 'should produce an error');
+      assert.equal(testValue.error.details[0].message, '"value" must be a valid BCP47 language tag',
+        'should produce a string error');
+    });
+
+    it('return error if bcp47 tag is not a string', () => {
+      const testValue = Joi.bcp47().validate(2);
+      assert.isOk(testValue.error, 'should produce an error');
+      assert.equal(testValue.error.details[0].message, '"value" must be a string', 'should produce a string error');
+    });
+  });
+});
